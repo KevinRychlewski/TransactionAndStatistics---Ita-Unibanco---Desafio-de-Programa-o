496b36388a41df90649c13d04b30b3e5af95f826
KevinRychlewski <kevinsipioni13@gmail.com>
2026-01-07 00:31:50 -0300

    feat: add transaction and statistics DTOs

---
+++ transaction-statistics/src/main/java/com/rychlewski/transaction_statistics/dto/StatisticsResponseDTO.java
@@ -0,0 +1,54 @@
+package com.rychlewski.transaction_statistics.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class StatisticsResponseDTO {
+
+    private BigDecimal sum = BigDecimal.ZERO;
+    private BigDecimal avg = BigDecimal.ZERO;
+    private BigDecimal max = BigDecimal.ZERO;
+    private BigDecimal min = BigDecimal.ZERO;
+    private long count = 0;
+
+    public BigDecimal getSum() {
+        return sum;
+    }
+
+    public void setSum(BigDecimal sum) {
+        this.sum = sum;
+    }
+
+    public BigDecimal getAvg() {
+        return avg;
+    }
+
+    public void setAvg(BigDecimal avg) {
+        this.avg = avg;
+    }
+
+    public BigDecimal getMax() {
+        return max;
+    }
+
+    public void setMax(BigDecimal max) {
+        this.max = max;
+    }
+
+    public BigDecimal getMin() {
+        return min;
+    }
+
+    public void setMin(BigDecimal min) {
+        this.min = min;
+    }
+
+    public long getCount() {
+        return count;
+    }
+
+    public void setCount(long count) {
+        this.count = count;
+    }
+
+}
---
+++ transaction-statistics/src/main/java/com/rychlewski/transaction_statistics/dto/TransactionRequestDTO.java
@@ -0,0 +1,34 @@
+package com.rychlewski.transaction_statistics.dto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public class TransactionRequestDTO {
+
+    @NotNull
+    @PositiveOrZero
+    private BigDecimal amount;
+
+    @NotNull
+    private Instant timestamp;
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Instant getTimeStamp() {
+        return timestamp;
+    }
+
+    public void setTimeStamp(Instant timeStamp) {
+        this.timestamp = timeStamp;
+    }
+}
